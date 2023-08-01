b5115743e2a470a57a4d996bb358fd9bc98dac80
Quentin Fox <quentinbennettfox@gmail.com>
2023-07-31 22:20:54 -0400

    keep tsconfig path validation as-is, fallback for baseUrl

--- src/utils/resolveAliasedImport.ts
+++ src/utils/resolveAliasedImport.ts
@@ -6,10 +6,6 @@ import {lilconfigSync} from 'lilconfig';
 const validate = {
     string: (x: unknown): x is string => typeof x === 'string',
     tsconfigPaths: (x: unknown): x is TsconfigPaths => {
-        if (x === undefined || x === null) {
-            return true;
-        }
-
         if (typeof x !== 'object' || x == null || Array.isArray(x)) {
             return false;
         }
@@ -28,7 +24,7 @@ const validate = {
     },
 };
 
-type TsconfigPaths = null | undefined | Record<string, string[]>;
+type TsconfigPaths = Record<string, string[]>;
 
 /**
  * Attempts to resolve aliased file paths using tsconfig or jsconfig
@@ -61,42 +57,38 @@ export const resolveAliasedImport = ({
     }
 
     const paths: unknown = config.config?.compilerOptions?.paths;
-    const baseUrl: unknown = config.config?.compilerOptions?.baseUrl;
+
+    const potentialBaseUrl: unknown = config.config?.compilerOptions?.baseUrl;
+
+    const baseUrl = validate.string(potentialBaseUrl) ? potentialBaseUrl : '.';
 
     const configLocation = path.dirname(config.filepath);
 
-    if (!validate.string(baseUrl)) {
-        return null;
-    }
+    if (validate.tsconfigPaths(paths)) {
+        for (const alias in paths) {
+            const aliasRe = new RegExp(alias.replace('*', '(.+)'), '');
 
-    if (!validate.tsconfigPaths(paths)) {
-        return null;
-    }
+            const aliasMatch = importFilepath.match(aliasRe);
 
-    for (const alias in paths) {
-        const aliasRe = new RegExp(alias.replace('*', '(.+)'), '');
+            if (aliasMatch == null) continue;
 
-        const aliasMatch = importFilepath.match(aliasRe);
+            for (const potentialAliasLocation of paths[alias]) {
+                const resolvedFileLocation = path.resolve(
+                    configLocation,
+                    baseUrl,
+                    potentialAliasLocation
+                        // "./utils/*" -> "./utils/style.module.css"
+                        .replace('*', aliasMatch[1]),
+                );
 
-        if (aliasMatch == null) continue;
+                if (!fs.existsSync(resolvedFileLocation)) continue;
 
-        for (const potentialAliasLocation of paths[alias]) {
-            const resolvedFileLocation = path.resolve(
-                configLocation,
-                baseUrl,
-                potentialAliasLocation
-                    // "./utils/*" -> "./utils/style.module.css"
-                    .replace('*', aliasMatch[1]),
-            );
-
-            if (!fs.existsSync(resolvedFileLocation)) continue;
-
-            return resolvedFileLocation;
+                return resolvedFileLocation;
+            }
         }
     }
 
-    // try to just use the baseUrl to resolve imports
-    // if not paths matched, still is a valid import
+    // fallback to baseUrl
     const resolvedFileLocation = path.resolve(
         configLocation,
         baseUrl,
